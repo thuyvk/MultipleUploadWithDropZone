0eb841a2ccf8306076930ddcdf8a1810b8c325b7
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2017-06-30 16:16:39 +0700

    update

--- DropZoneUploadMVC5/Controllers/HomeController.cs
+++ DropZoneUploadMVC5/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DropZoneUploadMVC5.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,8 +15,37 @@ namespace DropZoneUploadMVC5.Controllers
             return View();
         }
 
-        
+        [HttpPost, ValidateJsonAntiForgeryToken]
+        public ActionResult SaveUploadFile(int CategoryId)
+        {
+            var fName = string.Empty;
+            Photo result = new Photo();
 
+            foreach (string fileName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileName];
+                fName = file.FileName;
+                if(file != null && file.ContentLength > 0)
+                {
+                    string UploadFolder = "Category_" + CategoryId;
+                    var originalDirectory = new DirectoryInfo(string.Format("{0}Uploads", Server.MapPath(@"\")));
+                    string pathString = Path.Combine(originalDirectory.ToString(), UploadFolder);
+
+                    bool isExists = Directory.Exists(pathString);
+                    if (!isExists)
+                        Directory.CreateDirectory(pathString);
+
+                    string RandomName = Guid.NewGuid() + Path.GetExtension(fName);
+                    var path = string.Format("{0}\\{1}", pathString, RandomName);
+                    file.SaveAs(path);
+
+                    result.Name = fName;
+                    result.Url = "/Uploads/" + UploadFolder + "/" + RandomName;
+                }
+            }
+
+            return Json(new { Name = result.Name, UrlPhoto = result.Url }, JsonRequestBehavior.AllowGet);
+        }
 
     }
 }
---
+++ DropZoneUploadMVC5/Models/Photo.cs
@@ -0,0 +1,8 @@
+﻿namespace DropZoneUploadMVC5.Models
+{
+    public class Photo
+    {
+        public string Name { set; get; }
+        public string Url { set; get; }
+    }
+}
